26edb3ea39003c60d77a3b6d823f48370d4bfdd1
BarnMS <barnabymorleysmith@gmail.com>
2019-11-04 18:30:18 +0000

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,214 @@
+var grid = {};
+var colours = {}
+async function init() {
+  const colourResponse = await fetch('/colours');
+  colours = await colourResponse.json();
+  //console.log(colours);
+  Object.values(colours).forEach((colour, index) => {
+    var colourElem = document.querySelector(".colour[data-colour=\""+index+"\"]");
+    colourElem.style.backgroundColor = "#"+colour.hex;
+    colourElem.setAttribute("title", colour.name);
+    colourElem.addEventListener("click", () => { setColour(index, colour.hex) });
+  });
+  const timeoutResponse = await fetch('/timeout');
+  const timeoutJSON = await timeoutResponse.json();
+  if(timeoutJSON.auth != false) {
+    if(timeout == undefined)
+      timeout = new Date();
+    else
+      timeout = timeoutJSON.timeout;
+  }
+  const gridResponse = await fetch('/grid');
+  grid = await gridResponse.json();
+  //console.log(grid);
+  drawGrid();
+}
+init();
+
+async function setPixel(x, y, colour) {
+  var opts = {
+    x: x,
+    y: y,
+    c: colour
+  }
+  $.post("setColour", opts, function(data, status){
+    //console.log(data);
+    if(data.status <= 1) {
+      //console.log(Date.parse(data.timeout))
+      timeout = data.timeout;
+    }
+  });
+}
+
+var timeout = new Date();
+setInterval(() => {
+  if(Date.parse(timeout) > new Date()) {
+    document.getElementById("timeout").style.visibility = "visible";
+    //console.log(getTimeRemaining(timeout).minutes + ":" + getTimeRemaining(timeout).seconds)
+    document.getElementById("timeout").innerText = (getTimeRemaining(timeout).minutes + ":" + getTimeRemaining(timeout).seconds.toString().padStart(2, '0'));
+  } else {
+    document.getElementById("timeout").style.visibility = "hidden";
+  }
+}, 500)
+
+function getTimeRemaining(endtime){
+  var t = new Date(endtime).getTime() - new Date().getTime();
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+var offset = [0,0];
+var canvasCont = document.getElementById ("canvas");
+var canvasMove = document.getElementById ("canvasMove");
+
+var canvas = document.getElementById ("pixelCanvas");
+var ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = true;
+
+var isDown = false;
+var isMove = false;
+
+canvasCont.addEventListener('mousedown', function(e) {
+  isDown = true;
+  offset = [
+    canvasMove.offsetLeft - e.clientX,
+    canvasMove.offsetTop - e.clientY
+  ];
+}, true);
+
+var gridRef = [0, 0];
+canvas.addEventListener('mousemove', function(e) {
+  gridRef = [];
+  var childPos = $(canvasMove).offset();
+  var parentPos = $(canvasCont).offset();
+  var childOffset = {
+      top: childPos.top - parentPos.top,
+      left: childPos.left - parentPos.left
+  }
+  if(zoomed) {
+    gridRef = [
+      Math.ceil((e.clientX - childOffset.left - 271)/20) -1,
+      Math.ceil((e.clientY - childOffset.top - 15)/20) -1
+    ];
+    $("#pixelPreview").css({ left: (gridRef[0]*20), top: (gridRef[1]*20)+"px" })
+  } else {
+    gridRef = [
+      Math.ceil((e.clientX - childOffset.left - 271)/2) -1,
+      Math.ceil((e.clientY - childOffset.top - 15)/2) -1
+    ];
+    $("#pixelPreview").css({ left: (gridRef[0]*2), top: (gridRef[1]*2)+"px" })
+  }
+  //console.log(gridRef)
+  document.getElementById("position").innerText = gridRef.join(", ");
+}, true);
+
+document.addEventListener('mouseup', function(e) {
+  if(!isMove && e.srcElement.id == "pixelPreview") {
+    setPixel(gridRef[0], gridRef[1], e.srcElement.getAttribute('data-colour'))
+  }
+  isDown = false;
+  isMove = false;
+}, true);
+
+document.addEventListener('mousemove', function(e) {
+  event.preventDefault();
+  if (isDown) {
+    isMove = true;
+    var maxLeft = $('#canvas').innerWidth() - $('#canvasMove').outerWidth()
+    var maxTop = $('#canvas').innerHeight() - $('#canvasMove').outerHeight()
+    if(maxLeft > 0) canvasMove.style.left = Math.clamp(e.clientX + offset[0], 0, maxLeft) + 'px';
+    if(maxLeft <= 0) canvasMove.style.left = Math.clamp(e.clientX + offset[0], maxLeft, 0) + 'px';
+    if(maxTop > 0) canvasMove.style.top = Math.clamp(e.clientY + offset[1], 0, maxTop) + 'px';
+    if(maxTop <= 0) canvasMove.style.top = Math.clamp(e.clientY + offset[1], maxTop, 0) + 'px';
+  }
+}, true);
+window.addEventListener('resize', function(e) {
+  var maxLeft = $('#canvas').innerWidth() - $('#canvasMove').outerWidth()
+  var maxTop = $('#canvas').innerHeight() - $('#canvasMove').outerHeight()
+  if(maxLeft > 0) canvasMove.style.left = Math.clamp(canvasMove.offsetLeft, 0, maxLeft) + 'px';
+  if(maxLeft <= 0) canvasMove.style.left = Math.clamp(canvasMove.offsetLeft, maxLeft, 0) + 'px';
+  if(maxTop > 0) canvasMove.style.top = Math.clamp(canvasMove.offsetTop, 0, maxTop) + 'px';
+  if(maxTop <= 0) canvasMove.style.top = Math.clamp(canvasMove.offsetTop, maxTop, 0) + 'px';
+}, true);
+
+Math.clamp=function(a,b,c){return Math.max(b,Math.min(c,a));};
+
+var zoomed = false
+function zoomToggle() {
+  var newPos = []
+  if(zoomed) {
+    newPos[0] = -(canvasMove.offsetLeft - $(canvasCont).innerWidth()/2)/20
+    newPos[1] = -(canvasMove.offsetTop - $(canvasCont).innerHeight()/2)/20
+    canvasMove.style.width = "2000px";
+    canvasMove.style.height = "2000px";
+    $('#zoomToggle').text("zoom_in");
+    $("#pixelPreview").removeClass("big")
+
+  } else {
+    newPos[0] = -(canvasMove.offsetLeft - $(canvasCont).innerWidth()/2)/2
+    newPos[1] = -(canvasMove.offsetTop - $(canvasCont).innerHeight()/2)/2
+    canvasMove.style.width = "20000px";
+    canvasMove.style.height = "20000px";
+    $('#zoomToggle').text("zoom_out");
+    $("#pixelPreview").addClass("big")
+  }
+
+  zoomed = !zoomed
+
+  //console.log(newPos);
+
+  pickLocation(newPos[0], newPos[1])
+}
+
+function drawGrid(){
+    // Clear screen to white.
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,1000,1000);
+    for(var x = 0; x < 1000; x++) {
+      if(grid[x] == undefined) continue;
+      for(var y=0; y < 1000; y++) {
+        if(grid[x][y] == undefined) continue;
+        ctx.fillStyle = "#" + colours[grid[x][y]].hex;
+        ctx.fillRect( x, y, 1, 1 );
+      }
+    }
+}
+
+function setColour(index, hex) {
+  $("#pixelPreview").css({ background: "#"+hex })
+  $("#pixelPreview").attr("data-colour", index);
+}
+
+var source = new EventSource("pixelUpdates");
+source.onmessage = function(event) {
+  var data = JSON.parse(event.data);
+  if(grid[data.x] == undefined) { grid[data.x] = {} };
+  grid[data.x][data.y] = data.c
+  drawGrid();
+};
+
+function pickLocation(x, y) {
+  var goto = [x+0.5, y+0.5]
+  if(zoomed) {
+    canvasMove.style.left = $(canvasCont).innerWidth()/2-goto[0]*20+"px"
+    canvasMove.style.top = $(canvasCont).innerHeight()/2-goto[1]*20+"px"
+  } else {
+    canvasMove.style.left = $(canvasCont).innerWidth()/2-goto[0]*2+"px"
+    canvasMove.style.top = $(canvasCont).innerHeight()/2-goto[1]*2+"px"
+  }
+  var maxLeft = $('#canvas').innerWidth() - $('#canvasMove').outerWidth()
+  var maxTop = $('#canvas').innerHeight() - $('#canvasMove').outerHeight()
+  if(maxLeft > 0) canvasMove.style.left = Math.clamp(canvasMove.offsetLeft, 0, maxLeft) + 'px';
+  if(maxLeft <= 0) canvasMove.style.left = Math.clamp(canvasMove.offsetLeft, maxLeft, 0) + 'px';
+  if(maxTop > 0) canvasMove.style.top = Math.clamp(canvasMove.offsetTop, 0, maxTop) + 'px';
+  if(maxTop <= 0) canvasMove.style.top = Math.clamp(canvasMove.offsetTop, maxTop, 0) + 'px';
+}
---
+++ server.js
@@ -0,0 +1,197 @@
+var express  = require('express')
+  , session  = require('express-session')
+  , passport = require('passport')
+  , app      = express();
+
+
+var DiscordStrategy = require('passport-discord').Strategy
+  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+  , refresh = require('passport-oauth2-refresh');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var dScopes = ['identify', 'email'];
+var gScopes = ['email', 'profile', 'openid'];
+
+var discordStrat = new DiscordStrategy({
+    clientID: '640258963543425034',
+    clientSecret: '4iYvy2tx9O3aYtkyDPh8kaowfK1EDLw9',
+    callbackURL: 'http://localhost:80/auth/discord/callback'
+},
+function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+      return done(null, profile);
+  });
+});
+
+var googleStrat = new GoogleStrategy({
+  clientID: "371166401878-fql6j4b08fh1fq0uiur1r0im8cv8iq37.apps.googleusercontent.com",
+  clientSecret: "PFW4Y5g7GhzLYNSB999c6oNb",
+  callbackURL: "http://localhost:80/auth/google/callback",
+  scope: gScopes
+},
+function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+      return done(null, profile);
+  });
+}
+)
+
+passport.use(discordStrat);
+refresh.use(discordStrat);
+passport.use(googleStrat);
+refresh.use(googleStrat);
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"))
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
+
+var mcache = require('memory-cache');
+var cache = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key)
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.send
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
+app.get('/auth/discord', passport.authenticate('discord', { scope: dScopes }), function(req, res) {});
+app.get('/auth/discord/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { req.user.type = "d"; res.redirect('/') } // auth success
+);
+app.get('/auth/google', passport.authenticate('google', { scope: gScopes }), function(req, res) {});
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }), function(req, res) { req.user.type = "g"; res.redirect('/') } // auth success
+);
+app.get('/auth/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+app.get('/auth/info', checkAuth, function(req, res) {
+    //console.log(req.user)
+    res.json(req.user);
+});
+
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send({ auth: false });
+}
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get('/colours', function(req, res) {
+  res.send(colours);
+});
+
+app.post('/setColour', function(req, res) {
+  if(!req.isAuthenticated()) {
+    return res.send({ status: 3 });
+  }
+  var now = new Date();
+  if(timeouts[req.user.type + req.user.id] == undefined || new Date(timeouts[req.user.type + req.user.id]) < now) {
+    var body = req.body;
+    //console.log(req)
+    if(body.x < 0 || body.y < 0 || body.x > 999 || body.y > 999 || colours[body.c] == undefined)
+      return res.send({ status: 2 });
+
+    timeouts[req.user.type + req.user.id] = AddMinutesToDate(now, 1)
+    if(grid[body.x] == undefined) { grid[body.x] = {} }
+    if(grid[body.x][body.y] == body.c || (grid[body.x][body.y] == undefined && body.c == 0)) { return res.send({ status: 4 }); }
+    grid[body.x][body.y] = body.c;
+    //console.log(grid[body.x][body.y]);
+    res.send({ status: 0, timeout: timeouts[req.user.type + req.user.id] })
+    sse.send({ x: body.x, y: body.y, c: body.c });
+  } else {
+    res.send({ status: 1, timeout: timeouts[req.user.type + req.user.id] })
+  }
+});
+app.get('/timeout', checkAuth, function(req, res) {
+  res.send({ timeout: timeouts[req.user.type + req.user.id] });
+});
+
+app.get('/grid', cache(1), function(req, res) {
+  res.send(grid);
+});
+
+var SSE = require('express-sse');
+var sse = new SSE();
+
+app.get('/pixelUpdates', sse.init);
+
+app.listen(80, function (err) {
+    if (err) return console.log(err)
+    console.log('Listening at http://localhost:80/')
+})
+
+//Canvas Stuff
+const colours = [
+  {name: "White", hex: "ffffff"}, {name: "Light Grey", hex: "e4e4e4"}, {name: "Dark Grey", hex: "888888"}, {name: "Black", hex: "000000"},
+  {name: "Pink", hex: "ffa7d1"}, {name: "Red", hex: "e50000"}, {name: "Orange", hex: "e59500"}, {name: "Brown", hex: "a06a42"}, 
+  {name: "Yellow", hex: "e5d900"}, {name: "Light Green", hex: "94e044"}, {name: "Green", hex: "02be01"}, {name: "Aqua-Blue", hex: "00d3dd"},
+  {name: "Green-Blue", hex: "0083c7"}, {name: "Blue", hex: "0000ea"}, {name: "Violet", hex: "cf6ee4"}, {name: "Purple", hex: "820080"}
+];
+
+var grid = require("./grid.json");
+
+var fs = require("fs");
+
+var serverUp = true;
+function exitHandler(options, err) {
+  if(!serverUp){return;}
+  serverUp = false;
+  if(err) {
+    console.error(err);
+  }
+  fs.writeFileSync('grid.json', JSON.stringify(grid));
+  fs.writeFileSync('timeouts.json', JSON.stringify(timeouts));
+  //console.log("debug");
+  process.exit();   // Don't think you'll need this line any more
+}
+
+setInterval(() => {
+  fs.writeFileSync('grid.json', JSON.stringify(grid));
+  fs.writeFileSync('timeouts.json', JSON.stringify(timeouts));
+}, 300000)
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+var timeouts = require("./timeouts.json");
+
+function AddMinutesToDate(date,  minutes) {
+  return new Date(date.getTime() + minutes * 60000);
+}
